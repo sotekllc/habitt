7d71902c83b200f77114abf51ac9713a9739ba56
Sotek LLC <bailey@sotek.dev>
2025-09-16 14:33:15 -0700

    Placeholder for the HabitsFormScreen.

--- lib/habit/form.dart
+++ lib/habit/form.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
 
+import 'package:habitt/menu.dart';
+import 'package:habitt/theme.dart';
+
 class HabitsFormScreen extends StatefulWidget {
   const HabitsFormScreen({Key? key}) : super(key: key);
 
   @override
-  _FormState createState() => _FormState();
+  _HabitsFormScreenState createState() => _HabitsFormScreenState();
 }
 
-class _FormState extends State<HabitsFormScreen> {
+class _HabitsFormScreenState extends State<HabitsFormScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: theme.primaryColorDark,
+        title: const Text(
+          'Habits',
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: AppSizes.screenPadding,
+          child: const Text(
+            'HABITS FORM SCREEN',
+            style: TextStyle(
+              fontSize: 32,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      drawer: menuDrawer(context),
+    );
   }
 }
