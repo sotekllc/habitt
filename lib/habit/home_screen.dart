d9a4a23fe4afacf28349e246e23d281b629adb9a
Sotek LLC <bailey@sotek.dev>
2025-09-16 14:27:38 -0700

    Placeholder for HomeScreen.

--- lib/habit/home_screen.dart
+++ lib/habit/home_screen.dart
@@ -1,11 +1,41 @@
 import 'package:flutter/material.dart';
 
+import 'package:habitt/menu.dart';
+import 'package:habitt/theme.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: theme.primaryColorDark,
+        title: const Text(
+          'Habitt',
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: AppSizes.screenPadding,
+          child: const Text(
+            'HOME SCREEN',
+            style: TextStyle(
+              fontSize: 32,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      drawer: menuDrawer(context),
+    );
   }
 }
