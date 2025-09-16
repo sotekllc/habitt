6995be6fc256661fcfaa1bc310f199b3c6165ded
Sotek LLC <bailey@sotek.dev>
2025-09-16 14:27:22 -0700

    Adding placeholders for other screens.

---
+++ lib/habit/form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HabitsFormScreen extends StatefulWidget {
+  const HabitsFormScreen({Key? key}) : super(key: key);
+
+  @override
+  _FormState createState() => _FormState();
+}
+
+class _FormState extends State<HabitsFormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/reports/screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'package:habitt/menu.dart';
+import 'package:habitt/theme.dart';
+
+class ReportsScreen extends StatelessWidget {
+  const ReportsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: theme.primaryColorDark,
+        title: const Text(
+          'Weekly Report',
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: AppSizes.screenPadding,
+          child: const Text(
+            'REPORTS SCREEN',
+            style: TextStyle(
+              fontSize: 32,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      drawer: menuDrawer(context),
+    );
+  }
+}
