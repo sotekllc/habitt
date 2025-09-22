97a2a0c7223ed23400a409b6787cf4038bdb876d
Sotek LLC <bailey@sotek.dev>
2025-09-22 14:43:24 -0700

    Cleanup.

--- lib/user/register_screen.dart
+++ lib/user/register_screen.dart
@@ -264,24 +264,4 @@ class _RegisterScreenState extends State<RegisterScreen> {
       ),
     );
   }
-
-  // void showToast(FToast fToast, String text, Color color) {
-  //   Widget toast = Container(
-  //     padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-  //     decoration: BoxDecoration(
-  //       borderRadius: BorderRadius.circular(25.0),
-  //       color: color,
-  //     ),
-  //     child: Row(
-  //       mainAxisSize: MainAxisSize.min,
-  //       children: [Icon(Icons.check), SizedBox(width: 12.0), Text(text)],
-  //     ),
-  //   );
-
-  //   fToast.showToast(
-  //     child: toast,
-  //     gravity: ToastGravity.BOTTOM,
-  //     toastDuration: Duration(seconds: 2),
-  //   );
-  // }
 }
